5a67684129c07bbb1644e044cefad3bf1bdb3682
alalude <adesina.lalude@gmail.com>
2022-07-15 15:31:40 -0700

    Add files via upload

---
+++ src/main/java/com/final_project/stonk_api/StonkApiApplication.java
@@ -0,0 +1,13 @@
+package com.final_project.stonk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/final_project/stonk_api/StonkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_project.stonk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
