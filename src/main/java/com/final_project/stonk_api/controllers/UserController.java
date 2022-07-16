3e4d1049324d770a93cdb4f81307bbab1bbc5a92
Akinbiyi Lalude <adesina.lalude@gmail.com>
2022-07-16 15:25:52 -0700

    working on getting first route to work

--- src/main/java/com/final_project/stonk_api/controllers/UserController.java
+++ src/main/java/com/final_project/stonk_api/controllers/UserController.java
@@ -1,15 +1,22 @@
 package com.final_project.stonk_api.controllers;
 
+import com.final_project.stonk_api.entities.User;
 import com.final_project.stonk_api.repositories.UserRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+//    @Autowired
     private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @PostMapping("/add")
+    public User createNewUser(@RequestBody User user) {
+        return userRepository.save(user);
     }
 }
