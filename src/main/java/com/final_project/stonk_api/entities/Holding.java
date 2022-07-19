b9b4a4224514f165c37f6224eb4dbf7ed3894b38
unknown <curtisdhenley@gmail.com>
2022-07-19 19:02:42 -0400

    completed holding.java

---
+++ src/main/java/com/final_project/stonk_api/entities/Holding.java
@@ -0,0 +1,85 @@
+package com.final_project.stonk_api.entities;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Holding {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Integer id;
+
+    private String name;
+    private Double purchasePrice;
+    @CreationTimestamp
+    private Date purchaseDate;
+    private Double targetPrice;
+    private Integer portId = null;
+
+//    getters and setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Double getTargetPrice() {
+        return targetPrice;
+    }
+
+    public void setTargetPrice(Double targetPrice) {
+        this.targetPrice = targetPrice;
+    }
+
+    public Integer getPortId() {
+        return portId;
+    }
+
+    public void setPortId(Integer portId) {
+        this.portId = portId;
+    }
+
+
+//    constructors
+
+    public Holding() {
+
+    }
+
+    public Holding(HoldingDTO holdingDTO) {
+        this.name = holdingDTO.getName();
+        this.purchasePrice = holdingDTO.getPurchasePrice();
+        this.targetPrice = holdingDTO.getTargetPrice();
+    }
+}
