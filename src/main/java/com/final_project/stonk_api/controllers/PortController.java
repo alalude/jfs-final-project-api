ba4e609cea40b2f53957c783717ddf57e4ff7b6b
Akinbiyi Lalude <adesina.lalude@gmail.com>
2022-07-20 12:27:30 -0700

    added @CrossOrigin for quick push to Heroku from main

--- src/main/java/com/final_project/stonk_api/controllers/PortController.java
+++ src/main/java/com/final_project/stonk_api/controllers/PortController.java
@@ -7,6 +7,7 @@ import com.final_project.stonk_api.service.PortService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*") // allowing access from all ports
 @RequestMapping("/port")
 public class PortController {
 
